63e1c9f9f27af8e80a1fc75a9048207e88b7ff55
Ritankar Bhattacharjee <ritankar.bhattacharjee80@gmail.com>
2026-09-02 21:29:05 +0530

    updated the app version

--- app.config.ts
+++ app.config.ts
@@ -27,7 +27,7 @@ export default ({ config }: ConfigContext): ExpoConfig => ({
   ...config,
   name: getAppName(),
   slug: "worklog",
-  version: "1.7.5",
+  version: "1.8.0",
   orientation: "portrait",
   icon: "./assets/images/icon.png",
   scheme: "worklog",
